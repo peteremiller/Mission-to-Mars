c1e0b78c8c1ff6784a3230a7852c3cbba9f57cbe
Peter Miller <peteremiller@outlook.com>
2020-10-23 20:26:30 -0500

    Updating files.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, redirect
+from flask import render_template
 from flask_pymongo import PyMongo
 import scraping
-import os
 
 #Create an instance of Flask
 app = Flask(__name__)
@@ -14,9 +14,9 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
    #Find data
-   mars = mongo.db.mars_app.find_one()
+   mars = mongo.db.mars.find_one()
    #Return template and data
-   return render_template("templates/index.html", mars=mars)
+   return render_template("index.html", mars=mars)
 
 #Create route to scrape function
 @app.route("/scrape")
@@ -25,11 +25,10 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   
-   return "Scraping Successful!"
+   return redirect("/")
 
 if __name__ == "__main__":
-   app.run(debug= True)
+   app.run()
 
 
 
--- scraping.py
+++ scraping.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import  pandas as pd
 import datetime as dt
+import time
 
 def scrape_all():
     # Initiate headless driver for deployment
     browser = Browser("chrome", executable_path="chromedriver", headless=True)
-
     news_title, news_paragraph = mars_news(browser)
 
     # Run all scraping functions and store results in a dictionary
@@ -36,7 +33,7 @@ def mars_news(browser):
     browser.visit(url)
 
     # Optional delay for loading the page
-    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=3)
 
     # Convert the browser html to a soup object and then quit the browser
     html = browser.html
@@ -46,23 +43,24 @@ def mars_news(browser):
     try:
         slide_elem = news_soup.select_one("ul.item_list li.slide")
         # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        find("div", class_="content_title")
         news_title = slide_elem.find("div", class_="content_title").get_text()
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
-
     except AttributeError:
         return None, None
 
     return news_title, news_p
 
-
+### Featured Images
 def featured_image(browser):
     # Visit URL
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
+    time.sleep(2)
 
     # Find and click the full image button
-    full_image_elem = browser.find_by_id('full_image')[0]
+    full_image_elem = browser.find_by_id('full_image')
     full_image_elem.click()
 
     # Find the more info button and click that
@@ -92,24 +90,20 @@ def mars_facts():
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('http://space-facts.com/mars/')[0]
-
     except BaseException:
         return None
 
     # Assign columns and set index of dataframe
-    df.columns=['Description', 'Mars']
-    df.set_index('Description', inplace=True)
+    df.columns=['description', 'value']
+    df.set_index('description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
-
-if __name__ == "__main__":
-
-    # If running as script, print scraped data
-    print(scrape_all())
     
 def hemispheres(browser):
-    # 1. Use browser to visit the URL 
+    print(scrape_all())        
+ 
+  # 1. Use browser to visit the URL 
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
 
